dcfbf598bf1f93f5a1f06a302ab77da3e40b6e77
AlkaidSTART <2595006848@qq.com>
2026-04-18 15:39:01 +0800

    feat: 实现合并两个链表的功能，使用虚拟头节点和双指针方法

--- 链表/合并链表（虚拟头节点+双指针）.ts
+++ 链表/合并链表（虚拟头节点+双指针）.ts
@@ -9,5 +9,21 @@ class ListNode5 {
 }
 
 function mergeTwoLists(list1: ListNode5 | null, list2: ListNode5 | null): ListNode5 | null {
-  return list1;
+  const dummy = new ListNode5();
+  let tail = dummy;
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      tail.next = list1; // 添加节点
+      list1 = list1.next!;
+      tail = tail.next!;
+    } else {
+      tail.next = list2; // 添加节点
+      list2 = list2.next!;
+      tail = tail.next!;
+    }
+  }
+  // 将剩余的节点连接到结果链表
+  tail.next = list1 || list2;
+
+  return dummy.next;
 }
