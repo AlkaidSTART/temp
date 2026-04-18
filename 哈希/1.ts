a9472da877adab2ab53ae2ab8f391167351ab589
AlkaidSTART <2595006848@qq.com>
2026-04-18 12:28:33 +0800

    feat: 重构 twoSum 函数并新增 .prettierrc 配置文件

--- 哈希/1.ts
+++ 哈希/1.ts
@@ -1,11 +1,13 @@
-function twoSum(nums: number[], target: number): number[] {
+function twoSum1(nums: number[], target: number): number[] {
     const map=new Map<number,number>();
     for(let i=0;i<nums.length;i++){
         let diff=target-nums[i];
+        // 如果map中存在diff，说明有解
         if(map.has(diff)){
             return [map.get(diff)! ,i];
         }
         map.set(nums[i],i);
+        // 如果map中不存在diff，说明没有解
     }
 
 
