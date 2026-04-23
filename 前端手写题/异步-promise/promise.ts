0601ab5bdcb21a722e07e296469d91b6e19272d8
AlkaidSTART <2595006848@qq.com>
2026-04-23 14:22:17 +0800

    feat: 完善 MPromise 类，添加状态、值和原因的私有属性

--- 前端手写题/异步-promise/promise.ts
+++ 前端手写题/异步-promise/promise.ts
@@ -1,5 +1,9 @@
 type Status = "pending" | "fulfilled" | "rejected";
 
 class MPromise<T> {
+  private status: Status = "pending";
+  private value: T | undefined;
+  private reason: unknown;
+
   constructor() {}
 }
